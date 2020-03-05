b3a0bf2d64e9609117517aeefeac3ade0fa19707
mariolsuarez <marioleosuarez@gmail.com>
2020-03-05 17:09:33 -0500

    Fix: created check for empty list for pickNumbers class under N_numbers_from_list_seed.py file

--- RandomGenerator/N_numbers_from_list_seed.py
+++ RandomGenerator/N_numbers_from_list_seed.py
@@ -8,11 +8,15 @@ class PickNumbersSeed():
     def pickNumbers(theSeed, aList, rangeNum):
         random.seed(theSeed)
 
+        lst = aList
+        if not lst:
+            return "list is empty"
+
         newList = PickNumbersNoSeed.pickNumbers(aList, rangeNum)
 
         if not newList:
             return "list is empty"
-        try:
-            return newList
+
+        return newList
 
 #print(Pick_with_seed.pickNumbers(3, [34,45,67,89,123,44], 4))
